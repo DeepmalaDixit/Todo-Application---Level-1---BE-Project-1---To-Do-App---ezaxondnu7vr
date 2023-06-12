9f620e5916e3eeeb3ff93a1c24bc29bd0bb0719f
Deepmala <117112464+DeepmalaDixit@users.noreply.github.com>
2023-06-12 10:40:22 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -12,11 +12,25 @@ Complete your other field here
 
 var userSchema = mongoose.Schema({
   //Write your code here.
-},
-
-{ /* ... */ }
-
-);
+name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
 
 module.exports = mongoose.model("User", userSchema);
-
